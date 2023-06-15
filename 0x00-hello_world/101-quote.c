eb08363c15bd195e25a6f9627c09191c94be3d13
m-o-d-e <mohammed.jied@ump.ac.ma>
2023-06-15 16:44:31 -0700

    programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main
+ *
+ * description
+ *
+ * return: 1
+ *
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
